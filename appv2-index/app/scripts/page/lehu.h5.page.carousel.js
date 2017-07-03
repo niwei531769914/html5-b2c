78cc2e9a1013129acf3188fe68a05daf442c3f29
wangpen <931075365@qq.com>
2017-07-03 14:21:32 +0800

    [123]

--- appv2-index/app/scripts/page/lehu.h5.page.carousel.js
+++ appv2-index/app/scripts/page/lehu.h5.page.carousel.js
@@ -1,50 +1,49 @@
 define('lehu.h5.page.carousel', [
-        'can',
-        'zepto',
-        'fastclick',
-        'lehu.util',
-        'lehu.h5.framework.comm',
-        'lehu.h5.business.config',
-        'lehu.hybrid',
+		'can',
+		'zepto',
+		'fastclick',
+		'lehu.util',
+		'lehu.h5.framework.comm',
+		'lehu.h5.business.config',
+		'lehu.hybrid',
 
-        'lehu.h5.header.footer',
+		'lehu.h5.header.footer',
 
-        'lehu.h5.header.download',
+		'lehu.h5.header.download',
 
-        'lehu.h5.component.carousel'
-    ],
+		'lehu.h5.component.carousel'
+	],
 
-    function(can, $, Fastclick, util, LHFrameworkComm, LHConfig, LHHybrid,
-        LHFooter,LHDownload,
-        LHCarousel) {
-        'use strict';
+	function(can, $, Fastclick, util, LHFrameworkComm, LHConfig, LHHybrid,
+		LHFooter, LHDownload,
+		LHCarousel) {
+		'use strict';
 
-        Fastclick.attach(document.body);
+		Fastclick.attach(document.body);
 
-        var carousel = can.Control.extend({
+		var carousel = can.Control.extend({
 
-            /**
-             *
-             * [init 初始化]
-             * @param  {[type]} element 元素
-             * @param  {[type]} options 选项
-             */
-            init: function(element, options) {
+			/**
+			 *
+			 * [init 初始化]
+			 * @param  {[type]} element 元素
+			 * @param  {[type]} options 选项
+			 */
+			init: function(element, options) {
 
-                var param = can.deparam(window.location.search.substr(1));
+				var param = can.deparam(window.location.search.substr(1));
 
-                if(!param.hyfrom){
-                    // new LHDownload(null, {
-                    //     "position": "bottom"
-                    // });
-                    new LHDownload();
-                }
+				if(!param.hyfrom) {
+					// new LHDownload(null, {
+					//     "position": "bottom"
+					// });
+					new LHDownload();
+				}
 
-                var wheel = new LHCarousel("#content");
+				var wheel = new LHCarousel("#content");
 
+			}
+		});
 
-            }
-        });
-
-        new carousel('#content');
-    });
+		new carousel('#content');
+	});
