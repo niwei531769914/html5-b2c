5f18582e75fbb2bb678b7b8c7bbc1a83ad3825db
wangpen <931075365@qq.com>
2017-10-30 09:34:39 +0800

    commit modified

--- appv2-index/app/html/login/js/login.js
+++ appv2-index/app/html/login/js/login.js
@@ -1,15 +1,27 @@
 //页面rem
-(function (doc, win) {
-    var docEl = doc.documentElement,
-        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-        recalc = function () {
-            var clientWidth = docEl.clientWidth;
-            var clientWidth = $(".nwrapper").width();
-            /*if (!clientWidth) return;*/
-            docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+(function(doc, win) {
+	var docEl = doc.documentElement,
+		resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+		recalc = function() {
+			var clientWidth = docEl.clientWidth;
+			var clientWidth = $(".nwrapper").width();
+			/*if (!clientWidth) return;*/
+			docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
 
-        };
-    if (!doc.addEventListener) return;
-    win.addEventListener(resizeEvt, recalc, false);
-    doc.addEventListener('DOMContentLoaded', recalc, false);
+		};
+	if(!doc.addEventListener) return;
+	win.addEventListener(resizeEvt, recalc, false);
+	doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
+
+$.ajax({
+	method: "get",
+	url: "http://datainfo.duapp.com/shopdata/getGoods.php?callback=",
+	dataType: "JSONP",
+	beforeSend: function() {
+		 
+	},
+	success: function(data) {
+		 
+	}
+})
